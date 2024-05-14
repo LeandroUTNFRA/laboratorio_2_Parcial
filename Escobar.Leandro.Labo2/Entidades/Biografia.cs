e8bea42381c6696ce5b7a9216711c353492bfa1d
LeandroUTNFRA <122940722+LeandroUTNFRA@users.noreply.github.com>
2024-05-14 16:40:15 -0300

    Add files via upload

---
+++ Escobar.Leandro.Labo2/Entidades/Biografia.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Biografia : Publicacion
+    {
+        public override bool EsColor
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool HayStock 
+        {
+            get
+            {
+                return stock > 0;
+            }
+        
+        
+        }
+        public Biografia(string nombre) : base(nombre)
+        {
+        }
+
+        public Biografia(string nombre,int stock) : base(nombre,stock)
+        {
+        }
+        public Biografia(string nombre,int stock, float valor) : base(nombre, stock,valor)
+        {
+        }
+
+        public static explicit operator Biografia(string nombre)
+        {
+            return new Biografia(nombre);
+        }
+
+
+
+    }
+}
---
+++ Escobar.Leandro.Labo2/Entidades/Vendedor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Vendedor
+    {
+
+        private string nombre;
+        private List<Publicacion> ventas;
+
+        private Vendedor()
+        {
+            ventas = new List<Publicacion>();
+        }
+        public Vendedor(string nombre):this()
+        {
+            this.nombre = nombre;
+        }
+
+        public static string ObtenerInformeDeVentas(Vendedor vendedor)
+        {
+            float gananciaTotal = 0;
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{vendedor.nombre.ToUpper()}");
+            sb.AppendLine("-------------------------------");
+
+            foreach (Publicacion venta in vendedor.ventas)
+            {
+                sb.AppendLine($"Publicacion: {venta.ObtenerInformacionApp()}");
+                gananciaTotal+= venta.Importe;
+            }
+
+            sb.AppendLine($"Ganacia Total: {gananciaTotal}");
+
+            return sb.ToString();
+        }
+        public static bool operator  +(Vendedor vendedor, Publicacion publicacion)
+        {
+            if (publicacion.HayStock)
+            {
+                vendedor.ventas.Add(publicacion);
+                publicacion.Stock--;
+
+                return true;
+            }
+            return false;
+        }
+
+        
+    }
+}
